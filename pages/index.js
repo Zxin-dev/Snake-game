c936aec039e1be105cdfa89b6e601ae3429ef4bc
Болд Мөнх-эрдэнэ <HP233018@M301-CC0131.local>
2023-07-26 15:38:07 +0800

    alldone

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,10 @@
 import { useEffect, useState } from "react";
+import * as React from "react";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 import useInterval from "use-interval";
 const zoom = 15;
 const areaWidth = 40;
@@ -13,15 +19,82 @@ export default function Home() {
     left: getRandomInt(areaWidth - 1),
   });
   const [count, setCount] = useState(-1);
+  const [highScore, setHighScore] = useState("");
+
   const [body, setbody] = useState([
-    { top: 3, left: 11 },
-    { top: 3, left: 10 },
-    { top: 3, left: 9 },
+    { top: 3, left: 7 },
+    { top: 3, left: 6 },
+    { top: 3, left: 5 },
   ]);
+
+  const [speed, setSpeed] = useState("1");
+  const WallTypes = ["box", "vertical", "horizontal"];
+  const [locate, setlocate] = useState();
+  const Levels = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  const handleChange = (event) => {
+    setSpeed(event.target.value);
+  };
+  const handleChangeWall = (event) => {
+    if (event.target.value === "box") {
+      setlocate(box);
+    } else if (event.target.value === "vertical") {
+      setlocate(vertical);
+    }
+  };
+  const box = [
+    { top: 0, left: 1 },
+    { top: 0, left: 2 },
+    { top: 0, left: 3 },
+    { top: 0, left: 4 },
+    { top: 0, left: 5 },
+    { top: 0, left: 6 },
+    { top: 0, left: 7 },
+    { top: 0, left: 8 },
+    { top: 0, left: 9 },
+    { top: 0, left: 10 },
+    { top: 0, left: 11 },
+    { top: 0, left: 12 },
+    { top: 0, left: 13 },
+    { top: 0, left: 14 },
+    { top: 0, left: 15 },
+    { top: 0, left: 16 },
+  ];
+  const vertical = [
+    { top: 0, left: 1 },
+    { top: 0, left: 2 },
+    { top: 0, left: 3 },
+    { top: 0, left: 4 },
+    { top: 0, left: 5 },
+    { top: 0, left: 6 },
+    { top: 0, left: 7 },
+    { top: 0, left: 8 },
+    { top: 0, left: 9 },
+    { top: 0, left: 10 },
+    { top: 0, left: 11 },
+    { top: 0, left: 12 },
+    { top: 0, left: 13 },
+    { top: 0, left: 14 },
+    { top: 0, left: 15 },
+    { top: 0, left: 16 },
+    { top: 0, left: 17 },
+    { top: 0, left: 18 },
+    { top: 0, left: 19 },
+    { top: 0, left: 20 },
+    { top: 0, left: 21 },
+    { top: 0, left: 22 },
+    { top: 0, left: 23 },
+    { top: 0, left: 24 },
+    { top: 0, left: 25 },
+    { top: 0, left: 26 },
+    { top: 0, left: 27 },
+    { top: 0, left: 28 },
+    { top: 0, left: 29 },
+  ];
 
   const [direction, setDirection] = useState("right");
   useEffect((e) => {
     eatApple();
+    setHighScore(window.localStorage.getItem("Score"));
     window.addEventListener(`keydown`, (e) => {
       setDirection((prevDirection) => {
         switch (e.code) {
@@ -110,7 +183,6 @@ export default function Home() {
 
     if (newRight < 0) {
       newRight = areaWidth - 1;
-      console.log(newRight);
     }
     newbody.unshift({ ...newbody[0], left: newRight });
     setbody(newbody);
@@ -125,18 +197,6 @@ export default function Home() {
     newbody.unshift({ ...newbody[0], top: newBottom });
     setbody(newbody);
   }
-
-  // function goDown() {
-  //   const newbody = [...body];
-  //   for (let i = 0; i < newbody.length; i++) {
-  //     let newTop = newbody[i].top + 1;
-  //     if (newTop > areaHeight - 1) {
-  //       newTop = 0;
-  //     }
-  //     newbody[i] = { ...newbody[i], top: newTop };
-  //   }
-  //   setbody(newbody);
-  // }
   useInterval(() => {
     switch (direction) {
       case "right":
@@ -159,18 +219,89 @@ export default function Home() {
     }
     for (let i = 1; i < body.length; i++) {
       if (body[i].top === body[0].top && body[i].left === body[0].left) {
-        alert("game over");
+        if (count > window.localStorage.getItem("Score")) {
+          window.localStorage.setItem("Score", count);
+        }
         window.location.reload();
       }
     }
-  }, 1000 / body.length);
+  }, 1000 / speed);
+
   return (
     <main className={`flex min-h-screen flex-col items-center  p-24`}>
-      <div>score {count}</div>
+      <div
+        style={{
+          display: "flex",
+          width: areaWidth * zoom,
+          justifyContent: "space-between",
+          marginBottom: "10px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+
+            alignItems: "center",
+          }}
+        >
+          <div style={{ display: "flex" }}>
+            High score: <div style={{ fontWeight: "600" }}>{highScore}</div>
+          </div>
+          <div style={{ display: "flex", marginLeft: "10px" }}>
+            Score: <div style={{ fontWeight: "600" }}>{count}</div>
+          </div>
+        </div>
+        <div style={{ display: "flex" }}>
+          <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Speed</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={speed}
+                label="speed"
+                onChange={handleChange}
+                defaultValue={1}
+              >
+                {Levels.map((e) => {
+                  return <MenuItem value={e}>Level{e}</MenuItem>;
+                })}
+              </Select>
+            </FormControl>
+          </Box>
+          <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Wall</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                label="Wall"
+                onChange={handleChangeWall}
+              >
+                {WallTypes.map((e) => {
+                  return <MenuItem value={e}>{e}</MenuItem>;
+                })}
+              </Select>
+            </FormControl>
+          </Box>
+        </div>
+      </div>
       <div
         className="relative bg-slate-300"
         style={{ width: areaWidth * zoom, height: areaHeight * zoom }}
       >
+        {locate &&
+          locate.map((segment) => (
+            <div
+              className="absolute rounded bg-slate-900"
+              style={{
+                top: segment.top * zoom,
+                left: segment.left * zoom,
+                width: zoom,
+                height: zoom,
+              }}
+            ></div>
+          ))}
         {body.map((segment) => (
           <div
             className="absolute rounded bg-slate-900"
@@ -182,7 +313,6 @@ export default function Home() {
             }}
           ></div>
         ))}
-
         <div
           className="absolute rounded bg-slate-900"
           style={{
